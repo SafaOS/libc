5a3f1b686ff9d32ad588836a26baea8727a17491
ObserverUnit <116169309+ObserverUnit@users.noreply.github.com>
2025-04-08 04:34:02 +0200

    [build]: changed prefered optimization mode to ReleaseFast

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{
-        .preferred_optimize_mode = .ReleaseSmall,
+        .preferred_optimize_mode = .ReleaseFast,
     });
     const target = b.standardTargetOptions(.{ .default_target = .{
         .abi = .none,
