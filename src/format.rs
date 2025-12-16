e7d0359c981e07d37f51ac97188e0cbddea060a7
safiworks <116169309+safiworks@users.noreply.github.com>
2025-12-17 00:16:18 +0200

    fix(format): rewrote printf entirely

--- src/format.rs
+++ src/format.rs
@@ -1,9 +1,11 @@
-use core::ffi::{CStr, c_char, c_int, c_uint};
+use core::ffi::{VaArgSafe, c_char, c_int, c_long, c_longlong, c_short, c_uint};
+use core::ffi::{c_uchar, c_ulong, c_ulonglong, c_ushort};
 use core::fmt::Write;
 
 use safa_api::errors::ErrorStatus;
 
 use crate::errno::set_error;
+use crate::string::strlen;
 
 #[derive(Debug)]
 pub struct BufWriter<'a>(&'a mut [u8], usize);
@@ -41,35 +43,6 @@ impl<'a> Write for BufWriter<'a> {
     }
 }
 
-struct CWriterWrapper<'a, T: CWriter>(&'a mut T, usize);
-
-impl<'a, T: CWriter> CWriter for CWriterWrapper<'a, T> {
-    fn write_bytes(&mut self, bytes: &[u8]) -> Result<usize, ErrorStatus> {
-        let am = self.0.write_bytes(bytes)?;
-        self.1 += am;
-        Ok(am)
-    }
-}
-
-impl<'a, T: CWriter> Write for CWriterWrapper<'a, T> {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        map_error(self.write_bytes(s.as_bytes()).map(|_| ()))
-    }
-}
-
-fn until_fmt<'a, W: CWriter>(
-    writer: &mut W,
-    fmt: &'a [u8],
-) -> Result<(&'a [u8], usize), ErrorStatus> {
-    let mut current = fmt;
-    let mut wrote = 0;
-    while !fmt.is_empty() && fmt[0] != b'%' {
-        wrote += writer.write_byte(fmt[0])?;
-        current = &fmt[1..];
-    }
-    Ok((current, wrote))
-}
-
 fn map_error<O>(maybe: Result<O, ErrorStatus>) -> Result<O, core::fmt::Error> {
     maybe.map_err(|e| {
         set_error(e);
@@ -81,72 +54,322 @@ macro_rules! try_fmt {
     ($expr: expr) => {{ map_error($expr)? }};
 }
 
-pub fn printf_to<W: CWriter>(
-    writer: &mut W,
-    fmt: &[u8],
-    mut var_args: core::ffi::VaList,
-) -> Result<usize, core::fmt::Error> {
-    let mut current = fmt;
-    let mut writer = CWriterWrapper(writer, 0);
+struct CPrinter<'a, 'fmt, 'b, 'f, W: CWriter> {
+    writer: &'a mut W,
+    wrote: usize,
+    curr_index: usize,
+    fmt: &'fmt [u8],
+    var_args: core::ffi::VaList<'b, 'f>,
+}
 
-    while !current.is_empty() {
-        let (advanced, _) = try_fmt!(until_fmt(&mut writer, fmt));
-        current = advanced;
+impl<'a, 'fmt, 'b, 'f, T: CWriter> CWriter for CPrinter<'a, 'fmt, 'b, 'f, T> {
+    fn write_bytes(&mut self, bytes: &[u8]) -> Result<usize, ErrorStatus> {
+        let am = self.writer.write_bytes(bytes)?;
+        self.wrote += am;
+        Ok(am)
+    }
+}
 
-        if current.len() == 0 {
-            break;
-        }
-        current = &current[1..];
-        match current[0] {
-            b'%' => _ = try_fmt!(writer.write_byte(b'%')),
-            b'c' => _ = write!(writer, "{}", unsafe { var_args.arg::<u32>() as u8 as char })?,
-            b'z' => {
-                let usize = unsafe { var_args.arg::<usize>() };
-                match current[1] {
-                    b'd' => {
-                        let i = usize.cast_signed();
-                        write!(writer, "{}", i)?;
-                        current = &current[1..];
+impl<'a, 'fmt, 'b, 'f, T: CWriter> Write for CPrinter<'a, 'fmt, 'b, 'f, T> {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        map_error(self.write_bytes(s.as_bytes()).map(|_| ()))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum LengthModifier {
+    None,
+    /// hh
+    Char,
+    /// h
+    Short,
+    /// l
+    Long,
+    /// ll
+    LongLong,
+    /// L
+    LongDouble,
+    /// z
+    SizeT,
+    /// Z
+    PtrDiffT,
+    /// j
+    MaxT,
+}
+
+enum Kind {
+    Octal,
+    Hex,
+    BigHex,
+    Normal,
+}
+
+macro_rules! match_arg {
+    ($self:ident, $e: expr, { $($matched_e:pat => ($t0:ty, $t1:ty) $(as ($as0:ty, $as1:ty))?,)* }, $unsigned: expr, $precision: expr, $fmt_spec_no_prec:expr, $fmt_spec_prec:expr) => {
+        match $e {
+            $($matched_e => {
+                if !$unsigned {
+                    let arg = unsafe { $self.arg::<$t0>() $(as $as0)? };
+                    if let Some(prec) = $precision {
+                    write!($self, $fmt_spec_prec, arg, prec = prec)?;
+                    } else {
+                        write!($self, $fmt_spec_no_prec, arg)?;
                     }
-                    c => {
-                        _ = write!(writer, "{}", usize);
-                        if c == b'u' {
-                            current = &current[1..];
-                        }
+                } else {
+                    let arg = unsafe { $self.arg::<$t1>() $(as $as1)? };
+                    if let Some(prec) = $precision {
+                        write!($self, $fmt_spec_prec, arg, prec = prec)?;
+                    } else {
+                        write!($self, $fmt_spec_no_prec, arg)?;
                     }
                 }
-            }
-            b's' => {
-                let cstr_ptr = unsafe { var_args.arg::<*const c_char>() };
-                let cstr = unsafe { CStr::from_ptr(cstr_ptr) };
-                _ = try_fmt!(writer.write_bytes(cstr.to_bytes()));
-            }
-            b'x' => {
-                let uint = unsafe { var_args.arg::<c_uint>() };
-                _ = write!(writer, "{:x}", uint);
-            }
-            b'o' => {
-                let uint = unsafe { var_args.arg::<c_uint>() };
-                _ = write!(writer, "{:o}", uint);
-            }
-            b'X' => {
-                let uint = unsafe { var_args.arg::<c_uint>() };
-                _ = write!(writer, "{:X}", uint);
-            }
-            b'p' => {
-                let ptr = unsafe { var_args.arg::<*const ()>() };
-                _ = write!(writer, "{:?}", ptr);
-            }
-            b'u' => {
-                let uint = unsafe { var_args.arg::<c_uint>() };
-                _ = write!(writer, "{}", uint);
-            }
-            b'i' | b'd' => {
-                let int = unsafe { var_args.arg::<c_int>() };
-                _ = write!(writer, "{}", int);
-            }
-            _ => {}
+            }),*
+        }
+    };
+
+}
+
+impl<'a, 'fmt, 'b, 'f, W: CWriter> CPrinter<'a, 'fmt, 'b, 'f, W> {
+    fn new(writer: &'a mut W, fmt: &'fmt [u8], var_args: core::ffi::VaList<'b, 'f>) -> Self {
+        Self {
+            writer,
+            wrote: 0,
+            curr_index: 0,
+            fmt,
+            var_args,
         }
     }
-    Ok(writer.1)
+
+    #[inline]
+    pub fn peek(&self) -> Option<u8> {
+        self.fmt.get(self.curr_index).copied()
+    }
+
+    #[inline]
+    pub fn next(&mut self) -> Option<u8> {
+        let byte = self.peek();
+        if byte.is_some() {
+            self.curr_index += 1;
+        }
+        byte
+    }
+
+    unsafe fn arg<T: VaArgSafe>(&mut self) -> T {
+        unsafe { self.var_args.arg() }
+    }
+
+    fn next_int(
+        &mut self,
+        unsigned: bool,
+        kind: Kind,
+        precision: Option<usize>,
+        length: LengthModifier,
+    ) -> core::fmt::Result {
+        if length == LengthModifier::LongDouble {
+            let f = unsafe { self.arg::<f64>() };
+
+            return if let Some(prec) = precision {
+                write!(self, "{:.prec$}", f, prec = prec)
+            } else {
+                write!(self, "{}", f)
+            };
+        }
+
+        macro_rules! call_with {
+            ($fmt_no_prec:literal, $fmt_prec:literal) => {
+                match_arg!(
+                    self,
+                    length,
+                    {
+                    LengthModifier::None => (c_int, c_uint),
+                    LengthModifier::Char => (c_int, c_int) as (c_char, c_uchar),
+                    LengthModifier::Short => (c_int, c_int) as (c_short, c_ushort),
+                    LengthModifier::Long => (c_long, c_ulong),
+                    LengthModifier::LongLong => (c_longlong, c_ulonglong),
+                    LengthModifier::LongDouble => (usize, usize),
+                    LengthModifier::SizeT => (usize, usize),
+                    LengthModifier::PtrDiffT => (isize, isize),
+                    LengthModifier::MaxT => (isize, usize),
+                    },
+                    unsigned,
+                    precision,
+                    $fmt_no_prec,
+                    $fmt_prec
+                )
+            };
+        }
+
+        match kind {
+            Kind::Normal => call_with!("{}", "{:0prec$}"),
+            Kind::Octal => call_with!("{:o}", "{:0prec$o}"),
+            Kind::Hex => call_with!("{:x}", "{:0prec$x}"),
+            Kind::BigHex => call_with!("{:X}", "{:0prec$X}"),
+        };
+        Ok(())
+    }
+
+    fn try_make_length(&mut self) -> Option<LengthModifier> {
+        let Some(maybe_length) = self.peek() else {
+            return None;
+        };
+
+        match maybe_length {
+            b'h' => {
+                self.next();
+
+                Some(if self.peek() == Some(b'h') {
+                    self.next();
+                    LengthModifier::Char
+                } else {
+                    LengthModifier::Short
+                })
+            }
+            b'l' => {
+                self.next();
+
+                Some(if self.peek() == Some(b'l') {
+                    self.next();
+                    LengthModifier::LongLong
+                } else {
+                    LengthModifier::Long
+                })
+            }
+            b'j' => {
+                self.next();
+                Some(LengthModifier::MaxT)
+            }
+            b'z' => {
+                self.next();
+                Some(LengthModifier::SizeT)
+            }
+            b't' => {
+                self.next();
+                Some(LengthModifier::PtrDiffT)
+            }
+            b'L' => {
+                self.next();
+                Some(LengthModifier::LongDouble)
+            }
+            _ => None,
+        }
+    }
+    fn write_next_fmt(
+        &mut self,
+        length: Option<LengthModifier>,
+        precision: Option<usize>,
+    ) -> core::fmt::Result {
+        let Some(spec) = self.next() else {
+            return Ok(());
+        };
+
+        match spec {
+            b'c' => {
+                let c = unsafe { self.arg::<c_int>() } as c_char;
+                try_fmt!(self.write_byte(c as u8));
+                Ok(())
+            }
+            b'd' | b'i' => self.next_int(
+                false,
+                Kind::Normal,
+                precision,
+                length.unwrap_or(LengthModifier::None),
+            ),
+            b'u' => self.next_int(
+                true,
+                Kind::Normal,
+                precision,
+                length.unwrap_or(LengthModifier::None),
+            ),
+            b'o' => self.next_int(
+                true,
+                Kind::Octal,
+                precision,
+                length.unwrap_or(LengthModifier::None),
+            ),
+            b'x' => self.next_int(
+                true,
+                Kind::Hex,
+                precision,
+                length.unwrap_or(LengthModifier::None),
+            ),
+            b'p' => {
+                let ptr = unsafe { self.arg::<*const ()>() };
+                write!(self, "{ptr:?}")
+            }
+            b'X' => self.next_int(
+                true,
+                Kind::BigHex,
+                precision,
+                length.unwrap_or(LengthModifier::None),
+            ),
+            b's' => {
+                let ptr = unsafe { self.arg::<*const c_char>() };
+                let len = if let Some(prec) = precision {
+                    prec
+                } else {
+                    unsafe { strlen(ptr as *const _) }
+                };
+
+                let bytes = unsafe { core::slice::from_raw_parts(ptr as *const u8, len) };
+                try_fmt!(self.write_bytes(bytes));
+                Ok(())
+            }
+            b => {
+                if let Some(length) = length {
+                    self.next_int(false, Kind::Normal, precision, length)?;
+                    try_fmt!(self.write_byte(b));
+                }
+
+                Ok(())
+            }
+        }
+    }
+
+    fn write_all(mut self) -> Result<usize, core::fmt::Error> {
+        while let Some(byte) = self.next() {
+            if byte == b'%' {
+                let Some(maybe_spec) = self.peek() else {
+                    break;
+                };
+
+                match maybe_spec {
+                    b'%' => {
+                        self.next();
+                        try_fmt!(self.write_byte(b'%'));
+                    }
+                    b'.' => {
+                        _ = self.next();
+                        let Some(maybe_precision) = self.next() else {
+                            break;
+                        };
+
+                        let precision = match maybe_precision {
+                            b'*' => unsafe { self.arg::<c_uint>() as usize },
+                            p @ b'0'..b'9' => (p - b'0') as usize,
+                            _ => continue,
+                        };
+
+                        let length = self.try_make_length();
+                        self.write_next_fmt(length, Some(precision))?;
+                    }
+                    _ => {
+                        let length = self.try_make_length();
+                        self.write_next_fmt(length, None)?;
+                    }
+                }
+            } else {
+                try_fmt!(self.write_byte(byte));
+            }
+        }
+        Ok(self.wrote)
+    }
+}
+
+pub fn printf_to<'a, 'fmt, 'b, 'f, W: CWriter>(
+    writer: &'a mut W,
+    fmt: &'fmt [u8],
+    var_args: core::ffi::VaList<'b, 'f>,
+) -> Result<usize, core::fmt::Error> {
+    let printer = CPrinter::new(writer, fmt, var_args);
+    printer.write_all()
 }
