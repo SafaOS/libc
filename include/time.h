3eba2ec422a3ff7e400d63b71679d4cb93d95f03
safiworks <116169309+safiworks@users.noreply.github.com>
2026-06-02 01:53:15 +0300

    feat(time.h): gmtime_r

--- include/time.h
+++ include/time.h
@@ -16,6 +16,8 @@ struct tm {
     int tm_wday;
     int tm_yday;
     int tm_isdst;
+    long tm_gmtoff;
+    const char *tm_zone;
 };
 
 time_t time(time_t *time);
@@ -23,3 +25,19 @@ clock_t clock(void);
 
 time_t mktime(struct tm *time);
 #define difftime(t1, t2) ((double)(t1) - (double)(t2))
+
+struct tm *gmtime_r(time_t *time, struct tm *tm);
+static inline struct tm *localtime_r(time_t *time, struct tm *tm) {
+    // TODO: implement localtime_r
+    return gmtime_r(time, tm);
+}
+
+static inline struct tm *localtime(time_t *time) {
+    #include "stdlib.h"
+    struct tm *tm = (struct tm *)calloc(1, sizeof(struct tm));
+    return localtime_r(time, tm);
+}
+
+static inline struct tm *gmtime(time_t *time) {
+    return localtime(time);
+}
--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,4 @@
-use core::ffi::c_int;
+use core::ffi::{c_char, c_int, c_long};
 
 use crate::errno::set_error;
 
@@ -14,6 +14,8 @@ pub struct TM {
     pub tm_wday: c_int,
     pub tm_yday: c_int,
     pub tm_isdst: c_int,
+    pub tm_gmtoff: c_long,
+    pub tm_zone: *const c_char,
 }
 
 type TimeT = u64;
@@ -75,4 +77,60 @@ pub extern "C" fn gettimeofday(tv: *mut TimeVal, tz: *mut TimeZone) -> c_int {
         safa_api::printerrln!("gettimeofday(): TODO tz isn't null");
     }
     0
+}
+
+const fn is_leap_year(year: u32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn gmtime_r(timer: *const TimeT, result: *mut TM) -> *mut TM {
+    let time = unsafe { &*timer };
+    let result = unsafe { &mut *result };
+
+    // Credits to banan-OS
+    // https://git.bananymous.com/Bananymous/banan-os/src/branch/main/userspace/libraries/LibC/time.cpp
+    const MONTH_DAYS: [u64; 13] = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365];
+
+    let mut time = *time;
+    result.tm_sec = (time % 60) as c_int;
+    time = time / 60;
+    result.tm_min = (time % 60) as c_int;
+    time = time / 60;
+    result.tm_hour = (time % 24) as c_int;
+    time = time / 24;
+
+    let mut total_days = time;
+    result.tm_wday = (total_days + 4) as c_int % 7;
+    result.tm_year = 1970;
+    while total_days >= 365 || is_leap_year(result.tm_year as u32) {
+        total_days -= 365
+            + if is_leap_year(result.tm_year as u32) {
+                1
+            } else {
+                0
+            };
+        result.tm_year += 1;
+    }
+
+    let is_leap_day = is_leap_year(result.tm_year as u32) && total_days == MONTH_DAYS[2];
+    let had_leap_day = is_leap_year(result.tm_year as u32) && total_days > MONTH_DAYS[2];
+
+    for mon in 0..12 {
+        result.tm_mon = mon as c_int;
+        if total_days < MONTH_DAYS[mon + 1] || (is_leap_day || had_leap_day) {
+            break;
+        }
+    }
+
+    result.tm_mday = (total_days - MONTH_DAYS[result.tm_mon as usize]
+        + if !had_leap_day { 1 } else { 0 }) as c_int;
+    result.tm_yday = total_days as c_int;
+    result.tm_year -= 1900;
+    result.tm_isdst = 0;
+
+    result.tm_gmtoff = 0;
+    result.tm_zone = b"UTC\0".as_ptr() as *const c_char;
+
+    return result;
 }
