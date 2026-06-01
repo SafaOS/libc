b68d1b54fd0021d330b5dfcace22ba5fdf0f2f97
safiworks <116169309+safiworks@users.noreply.github.com>
2026-06-01 12:24:10 +0300

    fix(pthread.h)

--- include/pthread.h
+++ include/pthread.h
@@ -6,6 +6,7 @@
 typedef uint32_t pthread_t;
 typedef void pthread_attr_t;
 typedef void pthread_mutex_t ;
+typedef void pthread_mutexattr_t;
 
 pthread_t pthread_self(void);
 [[noreturn]] void pthread_exit(void *retval);
@@ -18,7 +19,7 @@ int pthread_attr_init(pthread_attr_t *attr);
 int pthread_attr_setstacksize(pthread_attr_t *attr, size_t stacksize);
 int pthread_attr_destroy(pthread_attr_t *attr);
 
-int pthread_mutex_init(pthread_mutex_t *mutex, const void *attr);
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
 int pthread_mutex_destroy(pthread_mutex_t *mutex);
 int pthread_mutex_lock(pthread_mutex_t *mutex);
 int pthread_mutex_unlock(pthread_mutex_t *mutex);
