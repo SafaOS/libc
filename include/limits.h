cb6c2e02e747adf5811f78b737f62c564ac4d0ac
safiworks <116169309+safiworks@users.noreply.github.com>
2026-06-19 00:49:51 +0300

    fix(limits): NAME_MAX too big

--- include/limits.h
+++ include/limits.h
@@ -13,7 +13,7 @@
 #define _POSIX_LINK_MAX                     8
 #define _POSIX_MAX_CANON                    255
 #define _POSIX_MAX_INPUT                    255
-#define _POSIX_NAME_MAX                     255
+#define _POSIX_NAME_MAX                     128
 #define _POSIX_OPEN_MAX                     256
 #define _POSIX_PATH_MAX                     256
 #define _POSIX_SYMLINK_MAX                  255
@@ -22,7 +22,7 @@
 // Runtime Invariant Values (Possibly Indeterminate)
 
 #define ATEXIT_MAX                    4096
-#define OPEN_MAX                      128
+#define OPEN_MAX                      255
 #define PAGESIZE                      PAGE_SIZE
 #define PTHREAD_STACK_MIN             PAGE_SIZE
 #define PTHREAD_THREADS_MAX           _POSIX_THREAD_THREADS_MAX
@@ -36,7 +36,7 @@
 #define LINK_MAX                 _POSIX_LINK_MAX
 #define MAX_CANON                _POSIX_MAX_CANON
 #define MAX_INPUT                _POSIX_MAX_INPUT
-#define NAME_MAX                 255
+#define NAME_MAX                 128
 #define PATH_MAX                 4096
 #define PIPE_BUF                 PAGE_SIZE
 
